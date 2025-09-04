3a9a6477c73c825fa292c2bebd5f10991e7ad1b1
LenyChang <manshitanyin2019@163.com>
2025-09-04 16:49:40 +0800

    Site updated: 2025-09-04 16:49:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/02/C语言“指针”的保姆级理解/"];function toRandomPost(){
+var posts=["2025/09/02/C语言“指针”的保姆级理解/","2025/09/04/分支语句if，switch/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
